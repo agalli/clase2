d51a9182d593b93bdb887a296ec7232d3c6eafaa
agalli <galli_ale@hotmail.com>
2016-02-05 17:00:06 -0300

    ejemplo final

--- www/js/index.js
+++ www/js/index.js
@@ -1,28 +1,39 @@
+var pages=null;
+var currentPage="Page1";
+
+loadPages=function(){
+    pages={
+        Page1:document.getElementById("Page1"),
+        Page2:document.getElementById("Page2")
+    }
+}
+document.addEventListener('DOMContentLoaded', function(){
+    loadPages();
+    FastClick.attach(document.body);
+}, false);
+
 var isOpenedMenu=false;
 
-var pagina1=document.getElementById("Page-1");
-var pagina2=document.getElementById("Page-2");
+/*var pagina1=document.getElementById("Page-1");
+var pagina2=document.getElementById("Page-2");*/
 
-var currentPage="Page-1";
+
 
 function menuButtonClick(){
     if(isOpenedMenu){
         isOpenedMenu=false;
-        pagina1.className="body transition center";
+        pages[currentPage].className="body transition center";
     }else{
-        pagina1.className="body transition right";
+        pages[currentPage].className="body transition right";
         isOpenedMenu=true;
     }
 }
-function menu(pagina){
-    if(pagina==="Page-1"){
-        pagina1.className="body visible transition center";
-        pagina2.className="hide";
+function menu(selectedPage){
+    pages[currentPage].className="hide";
+    pages[selectedPage].className="body right";
+    setTimeout(function(){
+       pages[selectedPage].className="body visible transition center";
+        currentPage=selectedPage;
         isOpenedMenu=false;
-    }else{
-        pagina2.className="body visible transition center";
-        pagina1.className="hide";
-        isOpenedMenu=false;
-    }
-    
+    },0);
 }
